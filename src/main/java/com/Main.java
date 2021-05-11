f719b8c1e1b6672b7e93f2c49f1f7b2cc7acfd9f
joyyue <live_orevil@126.com>
2021-05-11 23:41:10 +0800

    @joyyue @This is a TestController

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,13 +2,15 @@ package com;
 
 import com.WebClients.GreetingWebClient;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        GreetingWebClient greetingWebClient = new GreetingWebClient();
-        System.out.println(greetingWebClient.getRes());
+//        GreetingWebClient greetingWebClient = new GreetingWebClient();
+//        System.out.println(greetingWebClient.getRes());
+
     }
 }
